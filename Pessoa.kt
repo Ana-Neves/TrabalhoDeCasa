6d08ecfadbfc541e4d1da0569927bf7e1e7f52e5
Ana Caroline Rodrigues Neves <97240075+Ana-Neves@users.noreply.github.com>
2022-02-19 09:15:51 -0300

    Update Pessoa.kt

--- Pessoa.kt
+++ Pessoa.kt
@@ -6,14 +6,13 @@ open class Pessoa (
 
     // infos da mae
 
-    saldo: String,
     agencia:String,
     numeroDaConta:Byte,
     gerente:String,
     cliente:String,
 
 ):Conta(
-    saldo = saldo,
+ 
     agencia=agencia,
     numeroDaConta=numeroDaConta,
     gerente=gerente,
